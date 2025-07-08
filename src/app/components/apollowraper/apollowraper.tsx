cc09a0b7adb8977eec105926c5ea9ae198ceb623
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-07-08 13:51:56 +0300

    feat: create start page loyout, closes #6

--- src/app/components/apollowraper/apollowraper.tsx
+++
@@ -1,8 +0,0 @@
-'use client'; // <<<<<<< ЦЕ КРИТИЧНО ВАЖЛИВО!
-
-import { ApolloProvider } from '@apollo/client';
-import client from '@/lib/apollo'; // Імпортуємо ваш ApolloClient інстанс
-
-export function ApolloWrapper({ children }: { children: React.ReactNode }) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
-}
