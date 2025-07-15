f54ba3a9d155aa4a17f7ed4016b05a2c8ae5b982
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-07-15 19:41:32 +0300

    feat: add header to dashboard and change some layout meta data

---
+++ src/app/components/layout/header/header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import './header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1 className='header__tittle'>MEDIPANEL</h1>
+            <div className='header__links'>
+                <Image className='header__icon'  src="/icon/notification.svg" alt="Dashboard Growth" width={50} height={50} />
+                <Image className='header__icon' src="/icon/settings.svg" alt="Dashboard Growth" width={40} height={40} />
+                <Image className={"header__profile"} src="/icon/profile.svg" alt="Dashboard Growth" width={60} height={60} />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
